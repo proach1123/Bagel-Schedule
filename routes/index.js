f0ff19fbc55b6a2b4fe528dc16d64db3616f32d2
proach1123 <proach1123@gmail.com>
2017-02-01 13:30:40 -0500

    stuff

--- routes/index.js
+++ routes/index.js
@@ -82,7 +82,7 @@ module.exports = function(passport){
 		};
 	  	mongoDbFunctions.findDocuments({}, "Schedule", function(result){
 
-			/*weds = nextWed();
+			weds = nextWed();
 
 		  	for (var i=0; i <= weds.length-1; i++) {
 				for (var n=0; n<= result.length-1; n++){
@@ -90,14 +90,13 @@ module.exports = function(passport){
 						setupnames.push(result[n].setup.Name);
 					}
 				}	    	
-	    	}*/
+	    	}
 	    	res.render('homepage', 
 	    		{ setup1: result[0].setup.Name, 8301: result[0].eightthirty.Name, 9001: result[0].nine.Name, 9301: result[0].ninethirty.Name, 10001: result[0].ten.Name, clean11: result[0].cleanup1.Name, clean21: result[0].cleanup2.Name,
 	    		setup2: result[3].setup.Name, 8302: result[3].eightthirty.Name, 9002: result[3].nine.Name, 9302: result[3].ninethirty.Name, 10002: result[3].ten.Name, clean12: result[3].cleanup1.Name, clean22: result[3].cleanup2.Name,
 	    		setup3: result[1].setup.Name, 8303: result[1].eightthirty.Name, 9003: result[1].nine.Name, 9303: result[1].ninethirty.Name, 10003: result[1].ten.Name, clean13: result[1].cleanup1.Name, clean23: result[1].cleanup2.Name,
 	    		setup4: result[2].setup.Name, 8304: result[2].eightthirty.Name, 9004: result[2].nine.Name, 9304: result[2].ninethirty.Name, 10004: result[2].ten.Name, clean14: result[2].cleanup1.Name, clean24: result[2].cleanup2.Name
 	    	});
-
 	  	}); 
 	  	
 	});
@@ -152,6 +151,58 @@ module.exports = function(passport){
 
 	router.get('/output', isAuthenticated, function(req, res, next) {
 	  res.render('output', { user: req.user });
+	});
+
+	router.post('/output', /*isAuthenticated,*/ function(req, res, next){
+		
+		if(req.body){
+			var idList =[];
+			var wedarray=[];
+			var wednesdayArray = nextWed();
+  
+			for (var k = 0; k < wednesdayArray.length; k++){
+				var date = wednesdayArray[k];
+
+				//Algorithm stuff
+				mongoDbFunctions.algorithm("availability_Next", date, idList);
+  			}
+  
+
+
+			function nextWed(){
+				var l = 0;
+				var d = new Date();
+				var year = d.getFullYear();
+				var month = d.getMonth()+1;
+				var wedarray=[];
+
+				if(month==0||month==2||month==4||month==6||month==7||month==9||month==11){
+				    l=31;
+			    }
+
+			    else if(month!=1){
+				    l=30;
+				}
+
+				else if(year%4==0&&year%100==0&&year%1000!=0){
+					l=29;
+			    }
+
+			    else{
+				    l=28;
+			    }
+					    
+			    for (var i=0; i <= l; i++) {
+					console.log(i);
+					d = new Date(year, month, i, 0, 0, 0,0);
+					if (d.getDay() === 3){
+					  	wedarray.push(d);
+					}
+					    	
+	    		}
+				return wedarray;
+			};
+		}
 	});
 
 	router.get('/optout', isAuthenticated, function(req, res, next) {
